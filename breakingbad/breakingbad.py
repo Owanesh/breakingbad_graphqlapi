fdc46930ad4e6032536ab20b49e4da21c03ea0dc
Owanesh <24321561+Owanesh@users.noreply.github.com>
2021-02-23 01:30:35 +0100

    refactor: improves resolver making for deaths query

--- breakingbad/breakingbad.py
+++ breakingbad/breakingbad.py
@@ -6,10 +6,10 @@ from database import select_by_field, select_all
 from filters import DeathFilter, EpisodeFilter, QuoteFilter, CharacterFilter
 
 
-def make_resolver(
-    class_map: strawberry.type = None, filter_map: strawberry.type = UNSET
-) -> list:
-    def resolver(self, filters: filter_map = None):
+def make_resolver(class_map: strawberry.type = None,
+                  filter_map: strawberry.type = UNSET
+                  ) -> list:
+    def resolver(filters: filter_map = None):
         if not filters:
             return select_all(class_map=class_map)
         return select_by_field(class_map=class_map, filters=filters)
@@ -19,10 +19,19 @@ def make_resolver(
 @strawberry.type
 class Query:
     @strawberry.field
-    def deaths(self, info, filters: DeathFilter, responsible: typing.Optional[CharacterFilter] = None) -> typing.List[Death]:
+    def deaths(
+        self,
+        info,
+        filters: typing.Optional[DeathFilter] = None,
+        responsible: typing.Optional[CharacterFilter] = None,
+    ) -> typing.List[Death]:
         if responsible:
-            filters.responsible = select_by_field(class_map=Character, filters=responsible)[0].name
-        return select_by_field(class_map=Death, filters=filters)
+            if not filters:
+                filters: CharacterFilter = CharacterFilter()
+            filters.responsible = make_resolver(
+                class_map=Character, filter_map=responsible
+            )()[0].name
+        return make_resolver(class_map=Death, filter_map=filters)()
 
     episodes: typing.List[Episode] = strawberry.field(
         resolver=make_resolver(class_map=Episode, filter_map=EpisodeFilter)
@@ -34,5 +43,5 @@ class Query:
         resolver=make_resolver(class_map=Quote, filter_map=QuoteFilter)
     )
 
- 
+
 schema = strawberry.Schema(query=Query)
